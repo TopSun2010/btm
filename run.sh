9b7bb06114bdf45ce0f906de2980e6a221cb1c8e
TopSun2010 <44044190+TopSun2010@users.noreply.github.com>
2018-11-01 10:42:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-./HSPMiner -bpool btm.f2pool.com:9221 -bwal bm1q43kya8xnmfas5pk79nhhnj7656g0jxwn9mdgyq -bworker default -bpsw passd -logfile -api 127.0.0.1:16666
+#!/bin/bash
+sudo wget https://raw.githubusercontent.com/TopSun2010/btm/master/HSPMiner -O /bin/HSPMiner
+sudo chmod 777 /bin/HSPMiner
+sudo wget https://raw.githubusercontent.com/TopSun2010/btm/master/rc.local -O /etc/rc.local
+sudo chmod 777 /etc/rc.local
+sudo wget https://raw.githubusercontent.com/TopSun2010/btm/master/hsp -O /bin/hsp
+sudo chmod 777 /bin/hsp
+sudo wget https://raw.githubusercontent.com/TopSun2010/btm/master/showhsp -O /bin/showhsp
+sudo chmod 777 /bin/showhsp

1436e0f33d3d21572c3e0e3eb92373080d1bc8e6
TopSun2010 <44044190+TopSun2010@users.noreply.github.com>
2019-01-06 13:34:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo wget https://raw.githubusercontent.com/TopSun2010/btm/master/HSPMiner -O /bin/HSPMiner
 sudo chmod 777 /bin/HSPMiner
+
 sudo wget https://raw.githubusercontent.com/TopSun2010/btm/master/rc.local -O /etc/rc.local
 sudo chmod 777 /etc/rc.local
 sudo wget https://raw.githubusercontent.com/TopSun2010/btm/master/hsp -O /bin/hsp
